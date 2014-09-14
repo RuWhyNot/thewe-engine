c32f18b57a4064dd9566320772b10c085b76b3f9
Pavel Grebnev <mail@ruwhynot.com>
2014-09-14 23:42:48 +0400

    Revert "Удалены автогенерируемые файлы"
    This reverts commit 60f9afe9a627647b1056b4c93a5e213c18d60d38.

--- cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/ruwhynot/Projects/TheWe/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml : \
-/home/ruwhynot/Projects/TheWe/cocos2d/cocos/platform/android/java/AndroidManifest.xml \
